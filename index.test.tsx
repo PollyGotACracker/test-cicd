9e3b1f178451433aa00fd0a7827a8e667b0e9bb8
PollyGotACracker <bitterns96@gmail.com>
2026-03-24 00:45:12 +0900

    config(ci): ui 테스트 설정

---
+++ index.test.tsx
@@ -0,0 +1,13 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Input } from ".";
+
+test("X 버튼 클릭 시 onDelete props에 전달된 함수가 호출되는지 확인한다.", () => {
+  const onDelete = jest.fn();
+  render(<Input value="입력값" onChange={jest.fn()} onDelete={onDelete} />);
+
+  const deleteButton = screen.getByRole("button", { name: "입력값 지우기" });
+
+  fireEvent.click(deleteButton);
+
+  expect(onDelete).toHaveBeenCalled();
+});
